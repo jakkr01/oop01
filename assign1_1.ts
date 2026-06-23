0fa24aa6b2df653a13020b5e30a7bd96b437c44e
jakkr01 <cvoooth@gmail.com>
2026-06-23 20:31:34 +0700

    ส่งงาน Assignment1_1

--- assign1_1.ts
+++ assign1_1.ts
@@ -9,9 +9,9 @@ let hasDiscount: boolean = (totalPrice > 1000) || isMember;
 let discount: number = hasDiscount ? totalPrice * 0.10 : 0;
 let finalPrice: number = totalPrice - discount;
 
-console.log("========== รายงานสรุปยอดเงิน ==========");
+console.log(`========== รายงานสรุปยอดเงิน ==========`);
 console.log(`ชื่อสินค้า: ${productName} (จำนวนที่ซื้อ: ${quantity} ชิ้น)`);
 console.log(`ราคารวมทั้งหมด: ${totalPrice} บาท`);
 console.log(`ได้รับส่วนลดหรือไม่: ${hasDiscount}`);
 console.log(`ราคาที่ต้องจ่ายจริง: ${finalPrice} บาท`);
-console.log("=====================================");
+console.log(`=====================================`);
--- assign1_2.ts
+++ assign1_2.ts
@@ -4,24 +4,26 @@ let price: number = 550;
 
 let bookSummary: [string, number] = [bookTitle, price];
 
-
 let isAvailable: boolean = true;
 let discountRate: number = 0.15;
 let finalPrice: number = price * (1 - discountRate);
 
-
 let isPremium: boolean = price > 500;
-
 
 let canDisplay: boolean = isPremium && isAvailable;
 
-
-console.log("========== ระบบจัดการข้อมูลหนังสือ ==========)");
-console.log(`ชื่อหนังสือ: ${bookSummary[0]}`); 
+console.log("========== ระบบจัดการข้อมูลหนังสือ ==========");
+console.log(`ชื่อหนังสือ: ${bookSummary[0]}`);
 console.log(`รหัส ISBN: ${isbn}`);
-console.log(`ราคาปกติ: ${bookSummary[1]} บาท`); 
+console.log(`ราคาปกติ: ${bookSummary[1]} บาท`);
 console.log(`ราคาโปรโมชัน (ลด 15%): ${finalPrice.toFixed(2)} บาท`);
 console.log(`สถานะหนังสือพร้อมใช้งาน: ${isAvailable}`);
 console.log(`เป็นหนังสือระดับ Premium: ${isPremium}`);
-console.log(`คำแนะนำการจัดวาง: ${canDisplay ? "สามารถจัดแสดงที่หน้าตู้โชว์ได้" : "จัดวางที่ชั้นหนังสือปกติ"}`);
+console.log(
+  `คำแนะนำการจัดวาง: ${
+    canDisplay
+      ? "สามารถจัดแสดงที่หน้าตู้โชว์ได้"
+      : "จัดวางที่ชั้นหนังสือปกติ"
+  }`
+);
 console.log("=============================================");
